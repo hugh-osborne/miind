782be0f8646e1f92bde82a66a32c2f85718d1031
Hugh Osborne <hugh.osborne@gmail.com>
2019-11-05 17:06:07 +0000

    updates to c++ generation

--- apps/NDGridGenerator/Grid.hpp
+++ apps/NDGridGenerator/Grid.hpp
@@ -12,8 +12,6 @@ public:
     std::vector<double> dimensions;
     std::vector<unsigned int> resolution;
     std::vector<double> base;
-    std::vector<Point> points;
-    std::vector<Point> points_trans;
     std::vector<Cell> cells;
     std::vector<Cell> cells_trans;
 
@@ -25,42 +23,95 @@ public:
     timestep(_timestep) {
         num_dimensions = _dims.size();
 
-        // points = generate_points();
+        generate_cells(std::vector<unsigned int>(), resolution);
     }
 
-    // std::vector<Cell> generate_cells(std::vector<unsigned int> cell_coord, std::vector<unsigned int> res) {
-    //     unsigned int res_head = res[0];
+    // obviously this constrains the number of dimensions to a hard coded 3.
+    // need to pass the function as a parameter to the class. Can't be bothered right now.
+    void applyFunctionEuler(Point& p) {
+        double g_nap = 0.25;
+        double g_na = 30.0;
+        double g_k = 6.0;
+        double E_na = 55.0;
+        double E_k = -80.0;
+        double C = 1.0;
+        double g_l = 0.1;
+        double E_l = -64.0;
+        double I = 0.15;
+        double I_h = 0.0;
+
+        double v = p.coords[2];
+        double h_na = p.coords[1];
+        double m_k = p.coords[0];
+
+        double I_l = -g_l*(v - E_l);
+        double I_na = -g_na * h_na * (v - E_na) * (pow((pow((1 + exp(-(v+35)/7.8)),-1)),3));
+        double I_k = -g_k * (pow(m_k,4)) * (v - E_k);
+
+        double v_prime = I_na + I_k + I_l + I;
+
+        double part_1 = (pow((1 + (exp((v + 55)/7))),-1)) - h_na;
+        double part_2 = 30 / (exp((v+50)/15) + exp(-(v+50)/16));
+        double h_na_prime = part_1 / part_2;
+
+        double part_1 = (pow((1 + (exp(-(v + 28)/15))),-1)) - m_k;
+        double part_2 = 7 / (exp((v+40)/40) + exp((-v + 40)/50));
+        double m_k_prime = part_1 / part_2;
+
+        p.coords[0] = m_k + (timestep)*m_k_prime;
+        p.coords[1] = h_na + (timestep)*h_na_prime;
+        p.coords[2] = v + (timestep)*v_prime;
+
+    }
+
+    void generate_cells(std::vector<unsigned int> cell_coord, std::vector<unsigned int> res) {
+        unsigned int res_head = res[0];
+
+        if (res.size() <= 1) // We don't work with 1D
+            return;
+
+        std::vector<unsigned int> res_tail(res.size()-1);
+
+        for (unsigned int i=0; i<res_tail.size(); i++)
+            res_tail[i] = res[i+1];
         
-    //     std::vector<Cell> cells;
-    //     if (res.size() == 2) {
-    //         std::vector<unsigned int> res_tail(res.size()-1);
+        if (res.size() == 2) {
+            for (unsigned int d=0; d<res_head; d++) {
+                std::vector<unsigned int> full_coord(cell_coord.size()+1);
+                for (unsigned int c=0; c<cell_coord.size(); c++)
+                    full_coord[c] = cell_coord[c];
+                full_coord[cell_coord.size()] = d;
 
-    //         for (unsigned int i=0; i<res_tail.size(); i++)
-    //             res_tail[i] = res[i+1];
+                std::vector<double> base_point_coords(num_dimensions);
+                for (unsigned int j=0; j<num_dimensions; j++) {
+                    base_point_coords[j] = base[j] + (full_coord[j]*(dimensions[j]/resolution[j]));
+                }
 
-    //         for (unsigned int d=0; d<res_head; d++) {
-    //             std::vector<unsigned int> full_coord(cell_coord.size()+1);
-    //             for (unsigned int c=0; c<cell_coord.size(); c++)
-    //                 full_coord[c] = cell_coord[c];
-    //             full_coord[cell_coord.size()] = d;
+                std::vector<Point> points(pow(2,num_dimensions));
+                for (unsigned int i=0; i<pow(2,num_dimensions); i++) {
+                    std::vector<double> np(num_dimensions);
+                    for (unsigned int j=0; j<num_dimensions; j++){
+                        if ((i >> j) & 1 == 1)
+                            np[j] = base_point_coords[j] + (dimensions[j]/resolution[j]);
+                        else
+                            np[j] = base_point_coords[j];
+                    }
+                    Point p = Point(np);
+                    points.push_back(p);
+                }
+                cells.push_back(Cell(full_coord, num_dimensions, points, triangulator));
+            }
 
-    //             std::vector<Point> points;
-    //             std::vector<double> base_point_coords(num_dimensions);
-    //             for (unsigned int j=0; j<num_dimensions; j++) {
-    //                 base_point_coords[j] = base[j] + (full_coord[j]*(dimensions[j]/resolution[j]));
-    //             }
+            return;
+        }
 
-    //             for (unsigned int i=0; i<pow(2,num_dimensions); i++) {
-    //                 std::vector<double> np(num_dimensions);
-    //                 for (unsigned int j=0; j<num_dimensions; j++){
-    //                     if ((i >> j) & 1 == 1)
-    //                         np[j] += 
-    //                 }
-    //                 Point p = Point()
-    //                 points.push_back();
-    //             }
-                
-    //         }
-    //     }
-    // }
+        for (unsigned int d=0; d<res_head; d++){
+            std::vector<unsigned int> full_coord(cell_coord.size()+1);
+            for (unsigned int c=0; c<cell_coord.size(); c++)
+                full_coord[c] = cell_coord[c];
+            full_coord[cell_coord.size()] = d;
+            generate_cells(full_coord, res_tail);
+        }
+    }
+    
 };
